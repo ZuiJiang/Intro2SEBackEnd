42d3af200b8594515c6f440660a32e8cf9ddd182
yuanjingsong <532473857@qq.com>
2017-11-18 22:56:47 +0800

    update

--- BE/BE/settings.py
+++ BE/BE/settings.py
@@ -50,8 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'BE.urls'
